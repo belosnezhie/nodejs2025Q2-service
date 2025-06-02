f2b9ebb9f7eb8e2f14ea846caad48dc6fa9f3da5
Maria Treier <mariatraier@gmail.com>
2025-06-02 14:03:06 +0200

    feat: update yaml file, add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 4000;
@@ -12,7 +16,12 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const yamlFile = readFileSync(join(__dirname, '..', 'doc/api.yaml'), 'utf8');
+  SwaggerModule.setup('/docs', app, parse(yamlFile));
+
   await app.listen(PORT);
-  console.log('App is running on ' + PORT);
+  console.log(`App is running on http://localhost:${PORT}/`);
+  console.log(`To test API use http://localhost:${PORT}/docs`);
 }
 bootstrap();
